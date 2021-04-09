bd7ed0f4a71435debc420c318c083da85d4783bc
Milos Spasovski <spasovski_milos@hotmail.com>
2021-04-09 18:51:55 +0000

    Empty react project commit with Auth0 and Bootstrap dependencies

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# Cockatielreact
+Cockatielreact
+# Tamara test
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <>
+      <Button onClick={ () => console.log('button click')}>Yeah</Button>
+
+      
+      </>
+  );
+}
+
+export default App;
