595434247aa7e392055f04ac9083742cc1ccd585
David Dimitrevski <david@live.com>
2021-04-22 21:48:54 +0200

    Navbar/Footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import LoginButton from './Components/Authentication/LoginButton'
 import LogoutButton from './Components/Authentication/LogoutButton'
 import UserProfile from './Components/Authentication/UserProfile'
 import {useAuth0} from '@auth0/auth0-react'
-
+import UpperNavBar from './Components/UI/UpperNav'
+import Footer from './Components/UI/Footer'
 
 const App =() => {
 
@@ -13,9 +14,11 @@ const App =() => {
 
   return ( 
       <>
+      <UpperNavBar />             
         <LoginButton />
         <LogoutButton /> 
         <UserProfile />
+        <Footer />
       </>
   )
 }
