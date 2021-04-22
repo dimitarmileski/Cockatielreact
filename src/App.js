a4e948faf395d55f73cc8d9198b13c8c41422e81
Dimitar Mileski <dime.mileski@hotmail.com>
2021-04-22 23:40:39 +0200

    Created router component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import LoginButton from './Components/Authentication/LoginButton'
 import LogoutButton from './Components/Authentication/LogoutButton'
 import UserProfile from './Components/Authentication/UserProfile'
 import {useAuth0} from '@auth0/auth0-react'
-
-
+import PageRouter from './Components/Views/PageRouter'
+import { BrowserRouter as Router } from "react-router-dom"
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 const App =() => {
 
   const {isAuthenticated} = useAuth0()
@@ -16,6 +17,14 @@ const App =() => {
         <LoginButton />
         <LogoutButton /> 
         <UserProfile />
+
+        <Router>
+          <PageRouter />
+          <Link to="/Users">Users</Link>
+        </Router>
+
+
+     
       </>
   )
 }
