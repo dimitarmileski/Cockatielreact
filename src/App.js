e49adae52446dc2670e8d29184e2da52883c7c07
David Dimitrevski <david@live.com>
2021-04-29 21:16:01 +0200

    Conflict Resolved

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import LoginButton from './Components/Authentication/LoginButton'
 import LogoutButton from './Components/Authentication/LogoutButton'
 import UserProfile from './Components/Authentication/UserProfile'
 import {useAuth0} from '@auth0/auth0-react'
+import PageRouter from './Components/Views/PageRouter'
+import { BrowserRouter as Router } from "react-router-dom"
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import UpperNavBar from './Components/UI/UpperNav'
 import Footer from './Components/UI/Footer'
-
 const App =() => {
 
   const {isAuthenticated} = useAuth0()
@@ -14,11 +16,23 @@ const App =() => {
 
   return ( 
       <>
-      <UpperNavBar />             
+      
+        <LoginButton />
+        <LogoutButton /> 
+        <UserProfile />
+
+        <Router>
+          <PageRouter />
+          <Link to="/Users">Users</Link>
+        </Router>
+        <UpperNavBar />             
         <LoginButton />
         <LogoutButton /> 
         <UserProfile />
         <Footer />
+
+
+     
       </>
   )
 }
