c5cd381729756d8d41b4e88cf52f786216291b75
David Dimitrevski <david@live.com>
2021-05-06 20:47:50 +0200

    Updatejtirani footer i uppernav , dodadeni drop downs vo upper i search bar i dodadeno socialni mrezi broj i ime na brand vo footer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import LoginButton from './Components/Authentication/LoginButton'
 import LogoutButton from './Components/Authentication/LogoutButton'
 import UserProfile from './Components/Authentication/UserProfile'
 import {useAuth0} from '@auth0/auth0-react'
-import PageRouter from './Components/Views/PageRouter'
-import { BrowserRouter as Router } from "react-router-dom"
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import UpperNav from './Components/UI/UpperNav';
+import Footer from './Components/UI/Footer';
 const App =() => {
 
   const {isAuthenticated} = useAuth0()
@@ -17,12 +16,10 @@ const App =() => {
         <LoginButton />
         <LogoutButton /> 
         <UserProfile />
+        <UpperNav />
+        <Footer />
 
-        <Router>
-          <PageRouter />
-          <Link to="/Users">Users</Link>
-        </Router>
-
+      
 
      
       </>
--- src/Components/UI/UpperNav.js
+++ src/Components/UI/UpperNav.js
@@ -1,22 +1,43 @@
 import React from 'react'
-import {Nav,Form,FormControl,Button,Navbar}from'react-bootstrap'
-const UpperNav = () => {
-    return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+import {Nav,Form,FormControl,Button,Navbar,NavDropdown,}from'react-bootstrap'
+function UpperNav() {
+  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+
+  return (
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Дома</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    <NavDropdown title="Разгледај коктели" id="basic-nav-dropdown">
+        <NavDropdown.Item href="action/3.1">Нај популарни</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Филтер</NavDropdown.Item>
+      </NavDropdown>
+      <NavDropdown title="Креирај Костом Коктели" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Разгледај состојки</NavDropdown.Item>
+      </NavDropdown>
+      <NavDropdown title="За нас" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Тим</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Контакт</NavDropdown.Item>
+      </NavDropdown>
     </Nav>
     <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
+    <NavDropdown title="Моја Сметка" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+      <FormControl type="text" placeholder="Пребарај" className="mr-sm-2" />
+      <Button>Пребарај</Button>
     </Form>
-  </Navbar>
-        </div>
-    )
+    
+    
+  </Navbar.Collapse>
+</Navbar>
+  );
 }
+
 
 export default UpperNav
