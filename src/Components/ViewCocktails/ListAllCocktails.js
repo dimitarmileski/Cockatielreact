568d5a16a9e714f388104e9273d2df0e48d1484e
viktorijagoluzha <81702296+viktorijagoluzha@users.noreply.github.com>
2021-05-06 23:07:45 +0200

    Done ListAllCocktails.js page

--- src/Components/ViewCocktails/ListAllCocktails.js
+++ src/Components/ViewCocktails/ListAllCocktails.js
@@ -1,121 +1,113 @@
-import React, {Component} from 'react';
-import {Card, Button} from 'react-bootstrap'
-import GridList from '@material-ui/core/GridList';
-import Grid from "@material-ui/core/Grid";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import CardActions from "@material-ui/core/CardActions";
+import { useState } from 'react'
+import { Card, Button, Spinner } from 'react-bootstrap'
+import Grid from '@material-ui/core/Grid'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
 
+import { useAxiosGet } from '../../Components/Hooks/httprequest'
 
-class ListAllCocktails extends Component{
-    constructor(arg){
-        super(arg);
-        this.state = {
-            items:[],
-            isLoaded: false
-        };
-    }
+const ListAllCocktails = () => {
+  const [letter, setLetter] = useState('a')
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`
 
-    componentDidMount(){
-      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
-          .then(res=> res.json())
-          .then(({ drinks }) => {
-              this.setState({
-                  isLoaded: true,
-                  items: drinks,
-              })
-          });
+  const request = useAxiosGet(url)
+
+  let order = []
+
+  if (request.loading) {
+    return <div>Loading...</div>
   }
-  state = {
-    color: 'red'
-  }
-  imageClick = () => {
-    console.log('Click');
-  }     
-  onChange = () => {
-    this.setState({ color: 'green' });
- }
 
-    render(){
-        const {isLoaded, items} = this.state;
-        if(!isLoaded){
-            return <div>loading data...</div>;
-        }
-
-        else{           
-
-            return(
-               <div>
-                 <Container>
-                 <Typography
-                    color="textPrimary"
-                    gutterBottom
-                    variant="h4"
-                    align="center"
-                  >
-          Popular Drinks{" "}
+  if (request.data) {
+    return (
+      <Container>
+        <Typography
+          color="textPrimary"
+          gutterBottom
+          variant="h4"
+          align="center"
+        >
+          Popular Drinks
         </Typography>
-                 <Card>
-                <div className="Data">
-                  {/* <GridList> */}
-                    <div>
-                    <Grid container spacing = {3}>
-                      {/* <GridList className = "container"> */}
-                        {items.map(item=>(
-                         
-          
-                           
-                            <Card key={item.id}>
-                                <CardMedia color = "primary" img src={item.strDrinkThumb} >
-                                <img src={item.strDrinkThumb}width = "100" height = "100" onClick={this.imageClick} />
-                                <br/>
-                                 
-                                <CardContent>
-                                  <Typography color ="primary">
-                                    
-                                    </Typography>
-                                <Typography color="primary"  noWrap = "false" overflowWrap ="break-word" word-break = "break-all">
-                                {/* <div style={{ backgroundColor: item.strCategory }} */}
-                               
-                                 
-                                 <a href={item.strCategory} onClick={this.onChange}>{item.strCategory}</a>
-                                 {/* </div> */}
-                               
-                              </Typography>
-                              <Typography color="textSecondary" variant="subtitle2" overflowWrap ="break-word" word-break = "break-all">
-                              {item.strdrink +' '+ item.strInstructionsDE +' '} 
-                              </Typography>
-                              <Typography color="textSecondary" variant="subtitle2" overflowWrap ="break-word" word-break = "break-all">
-                              {item.idDrink}<br />
-                              </Typography>
-                              <Typography color="textSecondary" variant="subtitle2" overflowWrap ="break-word" word-break = "break-all">
-                              {item.dateModified} <br />
-                              </Typography>
-                                 
-                                 </CardContent>
-                                 <CardActions>
-                                  {/* <Button size="small" onClick="/home">{item.strCategory}</Button> */}
-                                </CardActions>
-                                 </CardMedia>   
+        <button onClick={() => setLetter('a')}>a</button>
+        <button onClick={() => setLetter('b')}>b</button>
+        <button onClick={() => setLetter('c')}>c</button>
+        <button onClick={() => setLetter('d')}>d</button>
 
-                            </Card>     
-                        ))};
-                        {/* </GridList> */}
-                        </Grid>
-                      
-                    </div>
-                 
-                </div>
-                </Card>
-                </Container>
-                </div>
-            );
-        }
+        <div>
+          <Grid container justify="space-evenly">
+            {request.data.drinks.map((item, index) => (
+              <Card key={index}>
+                <CardMedia color="primary" img src={item.strDrinkThumb}>
+                  <img src={item.strDrinkThumb} width="100" height="100" />
+                  <br />
+                  <CardContent>
+                    <Typography
+                      color="primary"
+                      noWrap="false"
+                      overflowWrap="break-word"
+                      word-break="break-all"
+                    >
+                      Име: {item.strDrink}
+                    </Typography>
 
-    }
-
+                    <Typography
+                      color="primary"
+                      noWrap="false"
+                      overflowWrap="break-word"
+                      word-break="break-all"
+                    >
+                      Категорија: {item.strCategory}
+                    </Typography>
+                    <Typography
+                      color="textSecondary"
+                      variant="subtitle2"
+                      overflowWrap="break-word"
+                      word-break="break-all"
+                    >
+                      <span style={{ fontWeight: '700' }}>Состојки:</span>
+                      <br />
+                      {` ${item.strIngredient1} `}
+                      <br />
+                      {` ${item.strIngredient2} `}
+                      <br />
+                      {` ${item.strIngredient3} `}
+                    </Typography>
+                    <Typography
+                      color="textSecondary"
+                      variant="subtitle2"
+                      overflowWrap="break-word"
+                      word-break="break-all"
+                    >
+                      <span style={{ fontWeight: '700' }}>Количина:</span>
+                      <br />
+                      {`${item.strMeasure1}`}
+                    </Typography>
+                  </CardContent>
+                  <Button
+                    onClick={() => {
+                      order.push({
+                        orderName: item.strDrink,
+                        orderCat: item.strCategory,
+                        dose: item.strMeasure1,
+                      })
+                      console.log(order)
+                    }}
+                  >
+                    Нарачај
+                  </Button>
+                </CardMedia>
+              </Card>
+            ))}
+          </Grid>
+        </div>
+      </Container>
+    )
+  } else {
+    return <></>
+  }
 }
 
 export default ListAllCocktails
