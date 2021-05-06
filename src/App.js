9264ea6189dc7d7d0be6b09f5bc636ad205c9ef8
Dimitar Mileski <dime.mileski@hotmail.com>
2021-05-06 20:59:04 +0200

    AboutUs component without PageRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginButton from './Components/Authentication/LoginButton'
 import LogoutButton from './Components/Authentication/LogoutButton'
 import UserProfile from './Components/Authentication/UserProfile'
 import {useAuth0} from '@auth0/auth0-react'
-import { BrowserRouter as Router, Route, Link  } from "react-router-dom"
-//import PageRouter from './Components/Views/PageRouter'
 import PageRouter from './Components/Views/PageRouter'
-
-
+import { BrowserRouter as Router } from "react-router-dom"
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 const App =() => {
 
   const {isAuthenticated} = useAuth0()
@@ -15,14 +13,19 @@ const App =() => {
   console.log(isAuthenticated)
 
   return ( 
-    <>
-    <LoginButton />
-    <LogoutButton /> 
-    <UserProfile />
+      <>
+        <LoginButton />
+        <LogoutButton /> 
+        <UserProfile />
 
-    <PageRouter/>
+        <Router>
+          <PageRouter />
+          <Link to="/Users">Users</Link>
+        </Router>
 
-  </>
+
+     
+      </>
   )
 }
 
--- src/Components/Views/PageRouter.js
+++ src/Components/Views/PageRouter.js
@@ -1,50 +1,19 @@
 import React from 'react'
 //import {Switch, Route} from 'react-router-dom'
-// import { BrowserRouter as Route, Switch } from "react-router-dom"
+import { BrowserRouter as Route, Switch } from "react-router-dom"
 import LoginButton from '../Authentication/LoginButton'
-import Home from './Home'
-import Coctails from './Coctails'
-import CreateCoctail from './CreateCoctail'
-import Profile from './Profile'
-import AboutUs from './AboutUs'
-import { BrowserRouter as Router, Route, Link, Switch  } from "react-router-dom"
-
 const PageRouter = () => {
     return (
 
         <div>
-            <Router>
-            <Link to="/">Home</Link>
-            <Link to="/Coctails">Coctails</Link>
-            <Link to="/CreateCoctail">CreateCoctail</Link>
-            <Link to="/AboutUs">AboutUs</Link>
-            <Link to="/Profile">Profile</Link>
-            
+            <Switch>
                 <Route exact path="/">
-                    <Home />
+                <LoginButton/>
                 </Route>
-               
-
-                <Route path="/Coctails">
-                    <Coctails />
+                <Route  path="/Users">
+                <LoginButton/>
                 </Route>
-               
-
-                <Route path="/CreateCoctail">
-                    <CreateCoctail />
-                </Route>
-            
-
-                <Route path="/AboutUs">
-                    <AboutUs />
-                </Route>
-                
-
-                <Route path="/Profile">
-                    <Profile />
-                </Route>
-                
-            </Router>
+            </Switch>
         </div>
     )
 }
