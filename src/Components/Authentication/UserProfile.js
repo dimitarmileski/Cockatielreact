0cc88f857bef0f50126e533f2895bd946bcc546c
Tamara Ilieva <tamarailieva@live.com>
2021-05-06 19:00:23 +0200

    Added recently purchased cocktails section

--- src/Components/Authentication/UserProfile.js
+++ src/Components/Authentication/UserProfile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useAuth0} from '@auth0/auth0-react'
 import {Card} from 'react-bootstrap'
+import CocktailCard from './CocktailCards/CocktailCard'
 
 
 const UserProfile = () => {
@@ -29,6 +30,22 @@ const UserProfile = () => {
                                 </div>
                             </div>
                         </Card.Text>
+
+                        <div> <b> Recently purchased: </b> </div>
+                        <div className="row">
+                        <CocktailCard
+                            Title="Mojito"
+                            Content="Mojito (/moʊˈhiːtoʊ/; Spanish: [moˈxito]) is a traditional Cuban highball. The cocktail often consists of five ingredients: white rum, sugar (traditionally sugar cane juice), lime juice, soda water, and mint."
+                        />
+                        <CocktailCard
+                            Title="Whiskey Sour"
+                            Content="This dependable drink is an easy fit for whiskey lovers, as well as those weary of the brown spirit: its lemony lift and slight sweetness make it appealing for citrus lovers, too. The simple recipe calls for whiskey, lemon juice, and sugar"
+                        />
+                        <CocktailCard
+                            Title="Martini"
+                            Content="The martini is a cocktail made with gin and vermouth, and garnished with an olive or a lemon twist. Over the years, the martini has become one of the best-known mixed alcoholic beverages. H. L. Mencken called the martini the only American invention as perfect as the sonnet and E. B."
+                        />
+                        </div>
                     </Card.Body>
                 </Card>
             </div>
