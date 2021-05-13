c4ab7f3f3e99a5ba8443c3a5dbe57752e293a968
Dimitar Mileski <dime.mileski@hotmail.com>
2021-05-14 00:44:42 +0200

    Design of Stripe payment

--- src/Components/Payment/prebuilt/SubmitButton.jsx
+++ src/Components/Payment/prebuilt/SubmitButton.jsx
@@ -5,7 +5,7 @@ const SubmitButton = styled.button`
   height: 40px;
   width: 100%;
   font-size: inherit;
-  background-color: ${props => (props.disabled ? "#7795f8" : "#f6a4eb")};
+  background-color: ${props => (props.disabled ? "#e17055" : "#e17055")};
   box-shadow: ${props =>
     props.disabled
       ? "none"
