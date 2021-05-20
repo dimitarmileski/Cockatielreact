58b1b423603e62b8f97c613079abcfef8676112d
viktorijagoluzha <81702296+viktorijagoluzha@users.noreply.github.com>
2021-05-20 23:45:02 +0200

    Design changes on ListAllCocktails.js page

--- src/Components/ViewCocktails/ListAllCocktails.js
+++ src/Components/ViewCocktails/ListAllCocktails.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Card, Button } from 'react-bootstrap'
 import Grid from '@material-ui/core/Grid'
 import CardMedia from '@material-ui/core/CardMedia'
@@ -6,15 +6,20 @@ import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 import InputAdornment from '@material-ui/core/InputAdornment'
-import SearchIcon from '@material-ui/icons/Search'
+import SearchIcon from '@material-ui/icons/Search';
 import FormControl from '@material-ui/core/FormControl'
 import Input from '@material-ui/core/Input'
 import Link from 'react-router-dom'
+import InputBase from '@material-ui/core/InputBase';
 import axios from 'axios'
+
+import GridList from '@material-ui/core/GridList';
 
 import { useAxiosGet } from '../Hooks/httprequest'
 
+
 const ListAllCocktails = () => {
+
   const [letter, setLetter] = useState('a')
 
   const handleChange = (event) => {
@@ -27,6 +32,7 @@ const ListAllCocktails = () => {
   const request = useAxiosGet(url)
 
   let order = []
+  
 
   if (request.loading) {
     return <div>Loading...</div>
@@ -35,20 +41,31 @@ const ListAllCocktails = () => {
   if (request.data) {
     return (
       <Container>
-        <Typography
+        <Typography style={{marginTop:"20px"}}
           color='textPrimary'
           gutterBottom
           variant='h4'
           align='center'
+          // fontStyle=""
         >
           Нај популарни
         </Typography>
 
         <div>
-          <div className='input-container'>
-            <input type='text' onChange={handleChange} />
-
-            <Grid container justify='space-evenly'>
+          <div className='input-container' style={{marginRight:"-40px"}} >
+            <input type='text' onChange={handleChange} 
+            placeholder="Search cocktail"
+           
+             style={{ width: "150px", float: "right",  marginBottom:"20px" }}
+           
+              />
+            
+            <Grid container item xs={12} spacing={3} >
+            
+              {/* <GridList padding={20}> */}
+              
+              
+           
               {request.data.drinks
                 .filter((item) => {
                   if (letter == '') {
@@ -60,29 +77,32 @@ const ListAllCocktails = () => {
                 })
 
                 .map((item, index) => (
-                  <Card key={index}>
-                    <CardMedia color='primary' img src={item.strDrinkThumb}>
-                      <img src={item.strDrinkThumb} width='100' height='100' />
+                  <Card style={{ width: "18rem" , padding:"10px", backgroundColor:'#ecf0f1'}} key={index}>
+                    <CardMedia color='primary' img src={item.strDrinkThumb}  >
+                      <img src={item.strDrinkThumb} width='100' height='100' style={{marginLeft:"45px"}} />
 
                       <br />
 
-                      <CardContent>
-                        <Typography
-                          color='primary'
+                      <CardContent  justify='space-evenly' style={{marginRight:"20px" }}>
+                        <Typography justify='space-evenly'
+                          color='orange'
                           noWrap='false'
                           overflowWrap='break-word'
                           word-break='break-all'
+                          fontWeight='bold'
                         >
-                          Име: {item.strDrink}
+                        
+                          <span style={{ fontWeight: '700' }}>   Име: {item.strDrink}</span>
                         </Typography>
 
                         <Typography
-                          color='primary'
+                          color='orange'
                           noWrap='false'
                           overflowWrap='break-word'
                           word-break='break-all'
                         >
-                          Категорија: {item.strCategory}
+                         
+                          <span style={{ fontWeight: '700' }}> Категорија:{item.strCategory}</span>
                         </Typography>
                         <Typography
                           color='textSecondary'
@@ -109,7 +129,7 @@ const ListAllCocktails = () => {
                           {`${item.strMeasure1}`}
                         </Typography>
                       </CardContent>
-                      <Button
+                      <Button variant="warning" style={{marginBottom:"30px",marginLeft:"30px"}} 
                         onClick={() => {
                           order.push({
                             orderName: item.strDrink,
@@ -131,6 +151,9 @@ const ListAllCocktails = () => {
                     </CardMedia>
                   </Card>
                 ))}
+            
+            {/* </GridList> */}
+             
             </Grid>
           </div>
         </div>
