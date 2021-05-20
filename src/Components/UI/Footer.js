b19734f050fa38114b5dc2eb0ad681f737f2a361
Milos Spasovski <spasovski_milos@hotmail.com>
2021-05-20 20:13:44 +0200

    Update Footer.js

--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -1,28 +1,26 @@
 import React from 'react'
-import { Navbar, Form, Nav, NavLink } from 'react-bootstrap'
+import {Navbar,Form,Nav, NavLink,}from'react-bootstrap'
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
-  return (
-    <Navbar bg='dark' variant='dark'>
-      <Navbar.Brand href='#home'>Cockatiel</Navbar.Brand>
-      <Nav className='mr-auto'></Nav>
-      <Form>
-        <Nav defaultActiveKey='/home' className='flex-column'>
-          <Nav class='text-white'>Социални мрежи:</Nav>
-          <Nav.Link className='text-primary' href='/home'>
-            Twitter
-          </Nav.Link>
-          <Nav.Link className='text-primary' href='/home'>
-            FaceBook
-          </Nav.Link>
-        </Nav>
-      </Form>
-      <Form>
-        <div className='ml-5 mb-1 text-white'>Контакт Број:</div>
-        <div className='ml-5 mt-8 text-white'>074 548 684</div>
-      </Form>
-    </Navbar>
-  )
+    return (
+        <Navbar  style={{background:'#e67e22'}}  >
+          <Navbar.Brand href="#home">Cockatiel</Navbar.Brand>
+          <Nav className="mr-auto">
+          </Nav>
+            <Form>
+          <Nav defaultActiveKey="/home" className="flex-column">
+            <form inline>
+            <Nav class="text-white">Социални мрежи:</Nav>
+            <SocialIcon url="https://twitter.com/" className="mr-3 mb-1" />
+            <SocialIcon url="https://facebook.com/" />
+            </form>
+
+          </Nav>
+          </Form>
+        </Navbar>
+
+    )
 }
 
 export default Footer
